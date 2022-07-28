328c1f5df6c740a9ebb399f8c19f95199964b03d
pedro <pedroarccortereal@gmail.com>
2022-07-28 16:43:38 +0100

    a correction to a decorator in annuities_certain.py

--- lifeactuary/annuities_certain.py
+++ lifeactuary/annuities_certain.py
@@ -1,5 +1,3 @@
-__author__ = "PedroCR"
-
 import numpy as np
 
 
@@ -16,14 +14,11 @@ class Annuities_Certain:
 
     def __init__(self, interest_rate, m=1):
         '''
-        This class instantiates the methods for the computation of financial annuities, for a given
-        interest rate and a chosen frequency of payments m (in each period of the interest rate)
 
-        :param interest_rate: interest rate, in percentage (e.g. use 5 for 5%)
-        :param m: frequency of payments, in each period of the interest rate
-
-        :return: several methods and annuities for the interest rate and frequency.
+        :param interest_rate:
+        :param m:
         '''
+
         self.interest_rate = interest_rate / 100.
         self.frequency = m
 
@@ -35,10 +30,10 @@ class Annuities_Certain:
     def check_terms(func):
         def func_wrapper(self, terms, *args, **kwargs):
             if not terms:
-                terms=0
+                terms = 0
             if terms < 0 or int(terms) != terms:
                 return np.nan
-            res = func(self, terms)
+            res = func(self, terms, *args, **kwargs)
             return res
 
         return func_wrapper
@@ -52,91 +47,47 @@ class Annuities_Certain:
 
         return func_wrapper
 
-    # Constant Term Financial Annuities
+    @check_terms
+    def aan(self, terms):
+        '''
+
+        :param terms:
+        :return:
+        '''
+        if not terms:
+            return 1 / self.dm
+        return (1 - np.power(self.vm, terms * self.frequency)) / self.dm
 
     @check_terms
     def an(self, terms):
         '''
-        Returns the present value of an immediate n-term financial annuity with payments equal to 1.
-        Payments are made in the end of the periods. In fractional annuities, payments of 1/m are made
-        m times per year at the end of the periods.
 
-        :param terms: number of years
-
-        :return: Expected Present Value (EPV) of an immediate n-term financial annuity
+        :param terms:
+        :return:
         '''
         if not terms:
             return 1 / self.im
         return (1 - np.power(self.vm, terms * self.frequency)) / self.im
 
     @check_terms
-    def aan(self, terms):
+    def Imaan(self, terms, payment=1, increase=1):
         '''
-        Returns the present value of a due n-term financial annuity with payments equal to 1.
-        Payments are made in the end of the periods. In fractional annuities, payments of 1/m are made
-        m times per year at the end of the periods.
 
-        :param terms: number of years
-
-        :return: Expected Present Value (EPV) of a due n-term financial annuity
+        :param terms:
+        :param payment:
+        :param increase:
+        :return:
         '''
-        if not terms:
-            return 1 / self.dm
-        return (1 - np.power(self.vm, terms * self.frequency)) / self.dm
-
-    # Variable Terms Financial Annuities
-    @check_terms
-    def Ian(self, terms, payment=1, increase=1):
-        '''
-        Returns the present value of an immediate $n$ term financial annuity with payments
-        increasing/decreasing arithmetically. Payments are made in the end of the periods.
-        First payment and increase amount may differ.
-        In fractional annuities, payments level within each interest period and increase/decrease from one
-        interest period to the next.
-
-        :param terms: number of years
-        :param payment: amount of the first payment
-        :param increase: increase amount of payments (positive for increasing annuities and negative for decreasing annuities)
-
-        :return: Expected Present Value (EPV) of an immediate arithmetically increasing/decreasing financial annuity. Payments level within each interest period and increase/decrease from one interest period to the next.
-        '''
-        if payment + increase * terms < 0:
-            return np.nan
-        # (payment - increase) * self.an(terms) + increase * (self.aan(terms) - terms * self.v ** terms) / self.im
-        return payment * self.an(terms) + increase / self.im * (
-                    (1 - self.v ** terms) / self.interest_rate - terms * self.v ** terms)
-
-    @check_terms
-    def Iaan(self, terms, payment=1, increase=1):
-        '''
-        Returns the present value of a due $n$ term financial annuity with payments increasing/decreasing arithmetically. Payments are made in the beginning of the periods.
-        First payment and increase amount may differ.
-        In fractional annuities, payments level within each interest period and increase/decrease from one
-        interest period to the next.
-
-        :param terms: number of years
-        :param payment: amount of the first payment
-        :param increase: increase amount of payments (positive for increasing annuities and negative for decreasing annuities)
-
-        :return: Expected Present Value (EPV) of a due arithmetically increasing/decreasing financial annuity. Payments level within each interest period and increase/decrease from one interest period to the next
-        '''
-        return self.Ian(terms, payment, increase) / self.vm
+        return self.Iman(terms, payment, increase) / self.vm
 
     @check_terms
     def Iman(self, terms, payment=1, increase=1):
         '''
-        Returns the present value of an immediate $n$-term financial annuity with payments
-        increasing/decreasing arithmetically.
-        Payments are made in the end of the periods.
-        First payment and increase amount may differ.
-        In fractional annuities, payments increase in each payment period.
 
-        :param terms: number of years
-        :param payment: amount of the first payment
-        :param increase: increase amount of payments (positive for increasing annuities and negative for decreasing annuities)
-
-        :return: Expected Present Value (EPV) of an arithmetically increasing/decreasing financial annuity.
-        Payments increase in each payment period and are paid in the end of periods.
+        :param terms:
+        :param payment:
+        :param increase:
+        :return:
         '''
         if payment + increase * terms < 0:
             return np.nan
@@ -147,36 +98,52 @@ class Annuities_Certain:
                / (self.frequency * self.v ** ((self.frequency - 1) / self.frequency) * (self.vm - 1) ** 2)
 
     @check_terms
-    def Imaan(self, terms, payment=1, increase=1):
-        '''
-        Returns the present value of an immediate $n$-term financial annuity with payments
-        increasing/decreasing arithmetically.
-        Payments are made in the beginning of the periods.
-        First payment and increase amount may differ.
-        In fractional annuities, payments increase in each payment period.
-
-        :param terms: number of years
-        :param payment: amount of the first payment
-        :param increase: increase amount of payments (positive for increasing annuities and negative for decreasing annuities)
-
-        :return: Expected Present Value (EPV) of an arithmetically increasing/decreasing financial annuity.
-        Payments increase in each payment period and are paid in the beginning of periods.
+    def Iaan(self, terms, payment=1, increase=1):
         '''
 
-        return self.Iman(terms, payment, increase) / self.vm
+        :param terms:
+        :param payment:
+        :param increase:
+        :return:
+        '''
+        return self.Ian(terms, payment, increase) / self.vm
+
+    @check_terms
+    def Ian(self, terms, payment=1, increase=1):
+        '''
+
+        :param terms:
+        :param payment:
+        :param increase:
+        :return:
+        '''
+        if payment + increase * terms < 0:
+            return np.nan
+        # (payment - increase) * self.an(terms) + increase * (self.aan(terms) - terms * self.v ** terms) / self.im
+        return payment * self.an(terms) + increase / self.im * (
+                (1 - self.v ** terms) / self.interest_rate - terms * self.v ** terms)
+
+    @check_grow
+    def Gaan(self, terms, payment=1, grow=0):
+        '''
+
+        :param terms:
+        :param payment:
+        :param grow:
+        :return:
+        '''
+        v = (1 + grow / 100) * self.v
+        return self.Gan(terms, payment, grow) / v ** (1 / self.frequency)
 
     @check_grow
     def Gan(self, terms, payment=1, grow=0):
         '''
-        Returns the present value of an immediate $n$ term financial annuity with payments increasing/decreasing geometrically. Payments are made in the end of the periods. In fractional annuities, payments level within each interest period and increase/decrease from one interest period to the next.
 
-        :param terms: number of years
-        :param payment: amount of the first payment
-        :param grow: growth rate from one year to the other
-
-        :return: Expected Present Value (EPV) of an arithmetically increasing/decreasing financial annuity. Payments increase in each year.
+        :param terms:
+        :param payment:
+        :param grow:
+        :return:
         '''
-
         v = (1 + grow / 100) * self.v
         if self.interest_rate == grow / 100:
             return payment * terms * self.frequency * self.vm / self.frequency
@@ -184,30 +151,25 @@ class Annuities_Certain:
                (1 - v ** (1 / self.frequency)) * v ** (1 / self.frequency) / self.frequency
 
     @check_grow
-    def Gaan(self, terms, payment=1, grow=0):
+    def Gmaan(self, terms, payment=1, grow=0):
         '''
-        Returns the present value of a due $n$ term financial annuity with payments increasing/decreasing geometrically. Payments are made in the beginning of the periods. In fractional annuities, payments level within each interest period and increase/decrease from one interest period to the next.
 
-        :param terms: number of years
-        :param payment: amount of the first payment
-        :param grow: growth rate from one year to the other
-
-        :return: Present Value of an arithmetically increasing/decreasing financial annuity. Payments increase in each year.
+        :param terms:
+        :param payment:
+        :param grow:
+        :return:
         '''
         v = (1 + grow / 100) * self.v
-        return self.Gan(terms, payment, grow) / v ** (1 / self.frequency)
+        return self.Gman(terms, payment, grow) / v ** (1 / self.frequency)
 
     @check_grow
     def Gman(self, terms, payment=1, grow=0):
         '''
-        Returns the present value of an immediate $n$ term financial annuity with payments increasing/decreasing geometrically. Payments are made in the end of the periods. In fractional annuities, payments increase in each payment period.
 
-        :param terms: number of years
-        :param payment: amount of the first payment
-        :param grow: rate of growing of payments, in percentage
-
-        :return: Present Value of an arithmetically increasing/decreasing financial annuity with terms paid in the end of periods.
-        Payments increase in each payment period.
+        :param terms:
+        :param payment:
+        :param grow:
+        :return:
         '''
         a1 = (1 - self.v) / self.im
         if self.interest_rate == grow / 100:
@@ -216,18 +178,3 @@ class Annuities_Certain:
         vg = 1 / (1 + ig)
         a2 = (1 - vg ** terms) / (1 - vg)
         return payment * a1 * a2
-
-    @check_grow
-    def Gmaan(self, terms, payment=1, grow=0):
-        '''
-        Returns the present value of an immediate $n$ term financial annuity with payments increasing/decreasing geometrically. Payments are made in the beginning of the periods. In fractional annuities, payments increase in each payment period.
-
-        :param terms: number of years
-        :param payment: amount of the first payment
-        :param grow: rate of growing of payments, in percentage
-
-        :return: Present Value of an arithmetically increasing/decreasing financial annuity with terms paid in the beginning of periods.
-        Payments increase in each payment period.
-        '''
-        v = (1 + grow / 100) * self.v
-        return self.Gman(terms, payment, grow) / v ** (1 / self.frequency)
